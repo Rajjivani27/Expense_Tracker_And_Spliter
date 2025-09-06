fdcf3a6a74fda7acc9d9f7d25844db944c46c5b9
Rajjivani27 <rajjivani027@gmail.com>
2025-09-06 23:50:54 +0530

    Added UserDetails model

--- expense_tracker/models.py
+++ expense_tracker/models.py
@@ -12,9 +12,14 @@ class CustomUser(AbstractBaseUser,PermissionsMixin):
     objects = CustomUserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['email','username']
+    REQUIRED_FIELDS = ['username']
 
     def __str__(self):
         return self.username
 
+class UserDetails(models.Model):
+    user = models.ForeignKey(CustomUser,on_delete=models.CASCADE,related_name="details")
+    name = models.CharField(max_length=100)
+    initial_bank_amount = models.IntegerField(default=0)
+    initial_cash_amount = models.IntegerField(default = 0)
 # Create your models here.
