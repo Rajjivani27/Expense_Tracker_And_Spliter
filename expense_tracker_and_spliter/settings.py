b610fcb70427583a31e9747e4c454a5c63d825f7
Rajjivani27 <rajjivani027@gmail.com>
2025-09-07 23:13:11 +0530

    Added some apps to INSTALLED_APPS and add expense_tracker urls to main urls

--- expense_tracker_and_spliter/settings.py
+++ expense_tracker_and_spliter/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'expense_tracker'
+    'expense_tracker',
+    'rest_framework',
 ]
 
 AUTH_USER_MODEL = 'expense_tracker.CustomUser'
