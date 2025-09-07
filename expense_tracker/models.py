357f5c780873e50df9d9e2e54c18801e3e1f6bcf
Rajjivani27 <rajjivani027@gmail.com>
2025-09-07 23:11:42 +0530

    Added some new models

--- expense_tracker/models.py
+++ expense_tracker/models.py
@@ -6,20 +6,48 @@ from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin
 class CustomUser(AbstractBaseUser,PermissionsMixin):
     email = models.EmailField(unique=True)
     username = models.CharField(unique=True)
+    name = models.CharField(max_length=100)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
     objects = CustomUserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    REQUIRED_FIELDS = ['username','name']
 
     def __str__(self):
         return self.username
 
-class UserDetails(models.Model):
-    user = models.ForeignKey(CustomUser,on_delete=models.CASCADE,related_name="details")
-    name = models.CharField(max_length=100)
-    initial_bank_amount = models.IntegerField(default=0)
-    initial_cash_amount = models.IntegerField(default = 0)
+class OnlineExpenseTracker(models.Model):
+    user = models.ForeignKey(CustomUser,on_delete=models.CASCADE,related_name="bank_account")
+    initial_amount = models.IntegerField(default=0)
+
+class CashExpenseTracker(models.Model):
+    user = models.ForeignKey(CustomUser,on_delete=models.CASCADE,related_name="cash_account")
+    initial_amount = models.IntegerField(default=0)
+
+class Expenses(models.Model):
+    expense_choises = [
+        ("rent","RENT"),
+        ("shopping","SHOPPING"),
+        ("food","FOOD"),
+        ("snacks","SNACKS"),
+        ("travel","TRAVEL"),
+        ("recharge","REACHARGE"),
+        ("grocery","GROCERY"),
+        ("dairy_item","DAIRY"),
+        ("vegetables","VEGES"),
+        ("miscellaneous","MSCL")
+    ]
+
+    payment_choices = [
+        ("online","ONLINE"),
+        ("cash","CASH"),
+        ("card","CARD")
+    ]
+
+    amount = models.IntegerField()
+    expense_type = models.CharField(choices=expense_choises,default="miscellaneous")
+    payment_type = models.CharField(choices=payment_choices,default="cash")
+    note = models.TextField()
 # Create your models here.
