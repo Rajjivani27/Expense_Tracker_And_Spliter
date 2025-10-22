50b55a75ec9fffcf9e370800630610c0542ef0f3
Rajjivani27 <rajjivani027@gmail.com>
2025-10-22 14:22:08 +0530

    Created a permission needed to handle friends APIs responses

--- expense_spliter/permissions.py
+++ expense_spliter/permissions.py
@@ -2,9 +2,23 @@ from rest_framework.permissions import BasePermission,SAFE_METHODS
 
 class IsAuthorOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
-        if self.action == SAFE_METHODS:
+        if request.method == SAFE_METHODS:
             return True
         
         return obj.requester == request.user
+    
+class IsAcceptingPersonOrReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method == SAFE_METHODS:
+            return True
+        
+        return obj.accepting_person == request.user
+    
+class IsParticipatingParty(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method == SAFE_METHODS:
+            return True
+        
+        return ((obj.person1 == request.user) or (obj.person2 == request.user))
 
 
